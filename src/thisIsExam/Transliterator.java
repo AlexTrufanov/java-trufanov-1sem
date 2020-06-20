6be7baa61061c766c4e37f048bc9562b7f9bba4a
AlexTrufanov <alex2002trufanov@gmail.com>
2020-06-20 18:57:01 +0300

    Экзамен, решена часть 2 и 3

--- src/thisIsExam/Transliterator.java
+++ src/thisIsExam/Transliterator.java
@@ -1,5 +1,10 @@
 package thisIsExam;
 
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Transliterator {
     char[] example;
     String[] translation;
@@ -11,9 +16,16 @@ public class Transliterator {
 
     String translateOneSymbol(char n) {
         for(int i = 0; i < example.length; i++) {
-            if(example[i] == n) {
+            if (example[i] == Character.toLowerCase(n)) {
+                if (Character.isUpperCase(n)) {
+                    if (translation[i].equals("")) {
+                        return "";
+                    }
+                    return String.valueOf(Character.toUpperCase(translation[i].charAt(0))) + translation[i].substring(1);
+                }
                 return translation[i];
             }
+
         }
         return String.valueOf(n);
     }
@@ -22,8 +34,22 @@ public class Transliterator {
         String[] chars = particle.split("");
         char[] symbol = particle.toCharArray();
         for (int i = 0; i < symbol.length; i++) {
-            particle = particle.replaceAll(chars[i], translateOneSymbol(symbol[i]));
+            particle = particle.replace(chars[i], translateOneSymbol(symbol[i]));
         }
         return particle;
+    }
+
+    public static Transliterator createICAO_DOC_9303() {
+        char[] example = new char[]{'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+        String[] translation = new String[]{"a", "b", "v", "g", "d", "e", "e", "zh", "z", "i", "i", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "ie", "y", "", "e", "iu", "ia"};
+        return new Transliterator(example, translation);
+    }
+
+    public void translateFile(String read, String print ) throws Exception {
+        String strings = Files.readString(Path.of(read));
+        strings = translate(strings);
+        try (PrintStream b = new PrintStream(print)) {
+            b.print(strings);
+        }
     }
 }
